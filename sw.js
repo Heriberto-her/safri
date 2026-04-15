9345b328e791dae1390e127bc8375eb8f57ed99d
Heriberto-her <safri961716@gmail.com>
2026-04-15 01:32:32 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,27 @@
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('fetch', e => {});
+function registrar(){
+
+let nombreInput = document.getElementById("nombre").value;
+let emailInput = document.getElementById("emailR").value;
+let passInput = document.getElementById("passR").value;
+let tipoInput = document.getElementById("tipoUser").value;
+
+auth.createUserWithEmailAndPassword(emailInput, passInput)
+.then(res => {
+
+  return db.collection("usuarios").doc(res.user.uid).set({
+    nombre: nombreInput,
+    tipo: tipoInput
+  });
+
+})
+.then(() => {
+  alert("Usuario creado correctamente 🔥");
+  volverLogin();
+})
+.catch(error => {
+  alert("Error: " + error.message);
+});
+
+}
